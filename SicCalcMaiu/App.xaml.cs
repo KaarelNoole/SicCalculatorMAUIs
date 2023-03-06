f8e74015f50f6c149a418be34efa6a4f47e2730f
TTHK\opilane <kaarelnoole@gmail.com>
2023-03-06 14:56:37 +0200

    MAUI Style and Colors

---
+++ SicCalcMaiu/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace SicCalcMaiu;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ SicCalcMaiu/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SicCalcMaiu;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
