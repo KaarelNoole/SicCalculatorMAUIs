b69fbb2b2437a07d4e3b5d1b9c42770772a6aeed
KaarelNoole <kaarelnoole@gmail.com>
2023-03-15 15:38:15 +0200

    Add Buttons

--- SicCalcMaiu/Views/CalculatorPage.xaml.cs
+++ SicCalcMaiu/Views/CalculatorPage.xaml.cs
@@ -7,5 +7,8 @@ public partial class CalculatorPage : ContentPage
 	public CalculatorPage()
 	{
 		InitializeComponent();
-	}
+
+        //init the view model
+        this.BindingContext = new CalculatorPageViewModel();
+    }
 }
